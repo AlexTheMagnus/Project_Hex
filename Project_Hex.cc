0efca9a40ea9966b27d6132fe06aeaa76ed61e84
AlexTheMagnus <i72lulem@uco.es>
2019-02-12 21:39:01 +0100

    Añadida portabilidad a otros sistemas GNU/Linux sin necesidad de variar el codigo y colores en terminal

--- Project_Hex.cc
+++ Project_Hex.cc
@@ -1,5 +1,8 @@
-//Debe ser ejecutado como admin "sudo ./a.out"
+//Debe ser ejecutado como admin "sudo ./Project_Hex.out"
 //ruta = "/usr/share/applications/"
+#include <unistd.h>
+#include <limits.h>
+
 #include <fstream>
 using std::ofstream;
 using std::ifstream;
@@ -9,18 +12,32 @@ using std::endl;
 #include <string>
 using std::string;
 
-int main() {
-  string app, icon;                          //nombre app, nombre del nuevo icono
-  string ruta = "/usr/share/applications/";   //ruta a la carpeta de las apps
-  //Formato del fichero de info:  nombre_archivo_app,nombre_icono_nuevo
-  string filenameinfo = "info.txt";          //nombre del fichero con la info de las apps a cambiar el icono
-  string linea;
+string getpath() {
+   char cwd[PATH_MAX];
+   if (getcwd(cwd, sizeof(cwd)) != NULL) {
+       string ruta_ejec(cwd);          //Paso de char* a string con el constructor de string
+       return (ruta_ejec + "/icons/");   //ruta donde se esta ejecutando el programa
+   }
+   else {
+       perror("getcwd() error");
+       return "error";
+   }
+}
 
-  ifstream fileinfo(filenameinfo);
-  if (!fileinfo){
-    cout << "Error al abrir el archivo de la info" << endl;
-    exit(-1);
-  }
+int main() {
+    cout << endl;
+
+    string app, icon;                          //nombre app, nombre del nuevo icono
+    string ruta = "/usr/share/applications/";   //ruta a la carpeta de las apps
+    //Formato del fichero de info:  nombre_archivo_app,nombre_icono_nuevo
+    string filenameinfo = "info.txt";          //nombre del fichero con la info de las apps a cambiar el icono
+    string linea;
+
+    ifstream fileinfo(filenameinfo);
+    if (!fileinfo){
+        cout << "Error al abrir el archivo de la info" << endl;
+        exit(-1);
+    }
 
   //getline devuelve falso cuando no hay mas lineas por leer
   while(getline(fileinfo, app, ',')){
@@ -29,11 +46,12 @@ int main() {
 
 
     app = ruta + app;
-    icon = "/home/alex/Pictures/Project_Hex/icons/" + icon;
+    icon = getpath() + icon;
     ifstream fileapp(app);                         //abre el archivo de dicha app
     ofstream fileapptemp(ruta + "filetemp.txt");   //abre el archivo temporalde la app
     if(!(fileapp.is_open()) || !(fileapptemp.is_open())){                  //no se pudo abrir alguno de los 2
       cout << "Error al abrir el archivo de la app" << endl;
+      cout << "Compruebe que haya asignado privilegios al programa" << endl;
       exit(-1);
     }
 
@@ -55,9 +73,16 @@ int main() {
 
 
     fileapp.close();
+    cout << "\e[0;32m";     //Pone las letras en color verde
     cout << "OK" << endl;
+    cout << "\e[0m";        //Reset de formato de letra (color, negrita,..)
   }
   fileinfo.close();
   cout << endl;
+  cout << "\e[1;32m";       //Pone las letras en color verde
   cout << "--Iconos cambiados correctamente. DONE.--" << endl;
+  //cout << "\e[0m";        //Reset de formato de letra (color, negrita,..)
+  //cout << "\e[0;34m" << endl;   //Color azul
+  //cout << "Pulse intro para terminar el programa" << endl;
+
 }
