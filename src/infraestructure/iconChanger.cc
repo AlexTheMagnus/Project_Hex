3181891c3687a33510b9b5208dece137217360e1
AlexTheMagnus <i72lulem@uco.es>
2020-11-08 18:58:23 +0100

    Finish and test iconChanger

--- src/infraestructure/iconChanger.cc
+++ src/infraestructure/iconChanger.cc
@@ -3,6 +3,9 @@
 #include <fstream>
 using std::ifstream;
 
+#include <limits.h>
+#include <unistd.h>
+
 #include <iostream>
 
 #include "../colorMacros.cc"
@@ -11,10 +14,47 @@ class NativeIconChanger : public IconChanger {
    private:
     string aptAppsPath = "/usr/share/applications/";
 
+    string tempFileName = "tempFile.txt";
+
+    // Methods -------------------------------------
+
+    string getAptAppsPath() { return aptAppsPath; }
+
+    string getTempFileName() { return "tempFile.txt"; }
+
+    string getSettingFileFullPath(string settingFileName) {
+        return getAptAppsPath() + settingFileName;
+    }
+
+    void printChangingIconMessage(string appName) {
+        std::cout << "Cambiando icono de " << appName << "..." << std::endl;
+    }
+
     void printErrorOpeningInfoFileMessage() {
         std::cout << RED;  //Color rojo
         std::cout << "Error al abrir el archivo" << std::endl;
         std::cout << RESET;  //Reset de formato de letra (color, negrita,..)
+    }
+
+    void printErrorGettingExecutionPathMessage() {
+        std::cout << RED;  //Color rojo
+        perror("getcwd() error");
+        std::cout << RESET;  //Reset de formato de letra (color, negrita,..)
+    }
+
+    string getIconFolderPath() {
+        char cwd[PATH_MAX];
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            string executionPath(cwd);           //Paso de char* a string con el constructor de string
+            return (executionPath + "/icons/");  //ruta donde se esta ejecutando el programa
+        } else {
+            printErrorGettingExecutionPathMessage();
+            return "error";
+        }
+    }
+
+    string getIconFullPath(string iconName) {
+        return getIconFolderPath() + iconName;
     }
 
     ifstream openFile(string fileNameInfo) {
@@ -27,30 +67,32 @@ class NativeIconChanger : public IconChanger {
         return fileInfo;
     }
 
-    string getAptAppsPath() { return aptAppsPath; }
+    void createNewAppFile(string settingFileName, string iconName) {
+        string fileLine;
+        std::ifstream settingFile = openFile(getSettingFileFullPath(settingFileName));
+        std::ofstream tempSettingFile(getAptAppsPath() + getTempFileName());
 
-    string settingFileFullPath(string settingFileName) {
-        return aptAppsPath + settingFileName;
+        while (getline(settingFile, fileLine, '\n')) {
+            if ((fileLine.compare(0, 5, "Icon=")) == 0) {
+                fileLine = "Icon=" + getIconFullPath(iconName);
+            }
+            tempSettingFile << fileLine << std::endl;
+        }
+
+        settingFile.close();
+        tempSettingFile.close();
     }
 
-    void createNewAppFile(ifstream &fileApp, string iconName) {
-        string fileLine;
-        ofstream fileAppTemp(appPath + "filetemp.txt");  //abre el archivo temporal de la app
-
-        while (getline(fileApp, fileLine, '\n')) {
-            if ((fileLine.compare(0, 5, "Icon=")) == 0) {  //comprueba si se encuentra en la linea de "Icon="
-                fileLine = "Icon=" + iconName;             //si entra cambia la ruta del icono
-            }
-            fileAppTemp << fileLine << std::endl;  //escribe en el nuevo fichero
-        }
+    void replaceAppFile(string settingFileName) {
+        remove(getSettingFileFullPath(settingFileName).c_str());
+        rename((getAptAppsPath() + getTempFileName()).c_str(), getSettingFileFullPath(settingFileName).c_str());
     }
 
    public:
     void changeIconOf(string settingFileName, string newIconName) {
-        ifstream settingFile = openFile(settingFileFullPath(settingFileName));
+        printChangingIconMessage(settingFileName);
 
-        createNewAppFile(settingFile, newIconName);
-
-        replaceAppFile(settingFile, fileAppTemp, settingFileName, appPath);
+        createNewAppFile(settingFileName, newIconName);
+        replaceAppFile(settingFileName);
     }
 };
