480c17625b02a508d07d5333c87d4b8d4d0d7552
AlexTheMagnus <i72lulem@uco.es>
2020-11-08 20:31:20 +0100

    Add controller and start coding main function

--- src/Project_Hex.cc
+++ src/Project_Hex.cc
@@ -1,37 +1,16 @@
-//Debe ser ejecutado como admin "sudo ./Project_Hex.out"
-//appPath = "/usr/share/applications/"
+#include <string.h>
 
-#include "getters.cc"
-#include "infraestructure/fileFunctions.cc"
-
-////////////////////////////////////////////////////////////////////////////////
-
-int main(int argc, char *argv[]) {
-    printWelcomeMessage();
-
-    string appName, iconName;                     //nombre app, nombre del nuevo icono
-    string appPath = "/usr/share/applications/";  //ruta a la carpeta de las apps
-    string iconFolderPath;                        //ruta de la carpeta icon
-    //Formato del fichero de info:  nombre_archivo_app,nombre_icono_nuevo
-    string fileInfoName = getFileInfoName(argc, argv);  //nombre del fichero con la info de las apps a cambiar el icono
-    string fileLine;
-
-    ifstream fileInfo = openFile("./" + fileInfoName);
-
-    //getline devuelve falso cuando no hay mas lineas por leer
-    while (getline(fileInfo, appName, ',')) {
-        getline(fileInfo, iconName, '\n');
-        printChangingIconMessage(appName);
-
-        appName = getApp(appPath, appName);
-        iconName = getIcon(iconFolderPath, iconName);
-
-        //modificar fichero de la app
-        updateAppFile(fileLine, iconName, appName, appPath);
-
-        printOKMessage();
+string getFileInfoName(int argc, char* argv[]) {
+    if (argc > 2) {
+        printUnexpectedArgumentsMessage();
+        exit(-1);
     }
+    if (argc = 2) {
+        return argv[1];
+    }
+    return "info.txt";
+}
 
-    fileInfo.close();
-    printEndMessage();
+int main(int argc, char* argv[]) {
+    string fileInfoName = getFileInfoName(argc, argv);
 }
